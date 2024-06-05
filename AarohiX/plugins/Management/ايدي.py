e54cfc9315fff4cc663050cb3d08d3560d7a06a1
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-05 12:54:18 +0300

    Update ايدي.py

--- AarohiX/plugins/Management/ايدي.py
+++ AarohiX/plugins/Management/ايدي.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import os
 import time
 import requests
@@ -10,26 +9,56 @@ from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 
 from AarohiX import app
-from random import  choice, randint
-
+from random import choice, randint
 
 @app.on_message(filters.command(["ايدي"], ""))
 async def madison(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
+    user_id = usr.id
+    username = usr.username
+    bio = usr.bio
+    group = await client.get_chat(message.chat.id)
+    
+    # تحقق من وجود صورة للمستخدم
+    user_photo = None
     async for photo in client.get_chat_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""╭⦿ᚐɴᴧᴍᴇ : {message.from_user.username}
-╰⦿ᚐᴜsᴇꝛ : {3}
+        user_photo = photo.file_id
+    
+    # إذا كانت هناك صورة، استخدمها في الرد
+    if user_photo:
+        await message.reply_photo(
+            user_photo,
+            caption=f"""╭⦿ᚐɴᴧᴍᴇ : {username}
+╰⦿ᚐᴜsᴇꝛ : {name}
 ╭⦿ᚐɪᴅ :  {user_id}
 ╰⦿ᚐʙɪᴏ : {bio}
 ⦿ᚐᴄʜᴧᴛ : {group.title}
-╰⦿ᚐᴄʜᴧᴛ ɪᴅ : {group.id}""", 
-        reply_markup=InlineKeyboardMarkup(
-            [
+╰⦿ᚐᴄʜᴧᴛ ɪᴅ : {group.id}""",
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        ),
-    )
+                    [
+                        InlineKeyboardButton(
+                            name, url=f"https://t.me/{username}")
+                    ],
+                ]
+            ),
+        )
+    else:
+        # إذا لم يكن هناك صورة، استخدم رسالة نصية
+        await message.reply_text(
+            f"""╭⦿ᚐɴᴧᴍᴇ : {username}
+╰⦿ᚐᴜsᴇꝛ : {name}
+╭⦿ᚐɪᴅ :  {user_id}
+╰⦿ᚐʙɪᴏ : {bio}
+⦿ᚐᴄʜᴧᴛ : {group.title}
+╰⦿ᚐᴄʜᴧᴛ ɪᴅ : {group.id}""",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            name, url=f"https://t.me/{username}")
+                    ],
+                ]
+            ),
+        )

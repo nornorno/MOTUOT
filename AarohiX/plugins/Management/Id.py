00a4bde7c5ffb7c448fa75df31603df43e641332
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-09 00:57:56 +0300

    Update Id.py

--- AarohiX/plugins/Management/Id.py
+++ AarohiX/plugins/Management/Id.py
@@ -1,53 +1,35 @@
-from AarohiX import app
 from pyrogram import Client, filters
-from pyrogram.enums import ParseMode
-
+from pyrogram.types import Message
 
 @app.on_message(filters.command('id'))
-async def getid(client, message):
+async def getid(client: Client, message: Message):
     chat = message.chat
     your_id = message.from_user.id
-    message_id = message.id
+    message_id = message.message_id  # تم تغيير 'id' إلى 'message_id' لتجنب الالتباس
     reply = message.reply_to_message
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+    text = f"**Message ID:** `{message_id}`\n"
+    text += f"**Your ID:** `{your_id}`\n"
 
-    if not message.command:
-        message.command = message.text.split()
+    # إزالة الأوامر المكررة لتقسيم النص
+    if len(message.command) >= 2:
+        user_id = (await client.get_users(message.command[1])).id
+        text += f"**User ID:** `{user_id}`\n"
 
-    if not message.command:
-        message.command = message.text.split()
+    text += f"**Chat ID:** `{chat.id}`\n\n"
 
-    if len(message.command) == 2:
-        try:
-            split = message.text.split(None, 1)[1].strip()
-            user_id = (await client.get_users(split)).id
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+    # تبسيط الشروط باستخدام 'getattr'
+    if reply and not getattr(reply, 'is_empty', True):
+        text += f"**Replied Message ID:** `{reply.message_id}`\n"
+        text += f"**Replied User ID:** `{reply.from_user.id}`\n\n"
 
-        except Exception:
-            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
+        if reply.forward_from_chat:
+            text += f"The forwarded channel, {reply.forward_from_chat.title}, has an ID of `{reply.forward_from_chat.id}`\n\n"
 
-    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
-
-    if (
-        not getattr(reply, "empty", True)
-        and not message.forward_from_chat
-        and not reply.sender_chat
-    ):
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
-
-    if reply and reply.forward_from_chat:
-        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
-        print(reply.forward_from_chat)
-
-    if reply and reply.sender_chat:
-        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
-        print(reply.sender_chat)
+        if reply.sender_chat:
+            text += f"ID of the replied chat/channel is `{reply.sender_chat.id}`"
 
     await message.reply_text(
         text,
-        disable_web_page_preview=True,
-        parse_mode=ParseMode.DEFAULT,
+        disable_web_page_preview=True
     )

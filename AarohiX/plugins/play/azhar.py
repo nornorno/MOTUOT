8fa751c688670a67e30ee84bce06438d547310b2
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-09 01:00:54 +0300

    Update azhar.py

--- AarohiX/plugins/play/azhar.py
+++ AarohiX/plugins/play/azhar.py
@@ -1,16 +1,9 @@
 import asyncio
-
 import random
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AarohiX import app
 
-from pyrogram.types import (InlineKeyboardButton,
-
-                            InlineKeyboardMarkup, Message)
-
-from strings.filters import command
-
-from pyrogram import filters, Client
 
 
 

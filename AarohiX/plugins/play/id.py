954f61ffbbf693357e312ad352f1c1f458f2ab07
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-09 00:14:48 +0300

    Update id.py

--- AarohiX/plugins/play/id.py
+++ AarohiX/plugins/play/id.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from AnonX import app
+from AarohiX import app
 import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton

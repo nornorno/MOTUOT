72292d1a754ea2b20871d1415fdbc7cf51a9614f
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-09 03:07:34 +0300

    Update بنك.py

--- AarohiX/plugins/play/بنك.py
+++ AarohiX/plugins/play/بنك.py
@@ -1,4 +1,4 @@
-'''
+
 import json
 import random
 from typing import List, Union
@@ -984,7 +984,7 @@ def top_money(client, message):
         response += f"@{account_username}: {account_balance} دولار\n"
     
     client.send_message(message.chat.id, response)
-       ''''
+    
 
 
 

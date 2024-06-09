01403d2609dcfab44f651668ec31ae7f05220257
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-09 15:08:07 +0300

    Update همسه.py

--- AarohiX/plugins/Management/همسه.py
+++ AarohiX/plugins/Management/همسه.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-app = Client("7107627916:AAExR51c8AKgmxvtpgj00kb2O9S3FwhaAqc")
+app = Client("my_bot")
 
 # متغير لتتبع حالة الرسالة
 state = {}

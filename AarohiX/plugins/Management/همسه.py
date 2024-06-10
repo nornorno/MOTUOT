7c8b18f47e391da5f5141735a733fd04b3d1d3ca
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-10 19:00:40 +0300

    Update همسه.py

--- AarohiX/plugins/Management/همسه.py
+++ AarohiX/plugins/Management/همسه.py
@@ -1,20 +1,22 @@
+import nest_asyncio
+nest_asyncio.apply()
+
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQueryResultArticle, InputTextMessageContent
 
 app = Client("my_bot")
 
-
 @app.on_inline_query()
 async def answer(client, inline_query):
     user = inline_query.from_user.username
-    title = f"هذه همسه سريه ل {user} هو فقط من يستطيع روئيتها"
-    message_text = f"اهذه همسه سريه ل {user} هو فقط من يستطيع روئيتها"
+    title = f"هذه همسة سرية لـ {user}، هو فقط من يستطيع رؤيتها"
+    message_text = f"هذه همسة سرية لـ {user}، هو فقط من يستطيع رؤيتها"
     results = [
         InlineQueryResultArticle(
             title=title,
             input_message_content=InputTextMessageContent(message_text),
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("اظغط للرؤيه", callback_data=f"{user}or{message_text}")
+                InlineKeyboardButton("اضغط للرؤية", callback_data=f"{user}or{message_text}")
             ]])
         )
     ]
@@ -28,17 +30,17 @@ async def callback_query_answer(client, callback_query: CallbackQuery):
         if callback_query.from_user.username in ex:
             await callback_query.answer(ex[1], show_alert=True)
         else:
-            await callback_query.answer("الرساله ليست لك", show_alert=True)
+            await callback_query.answer("الرسالة ليست لك", show_alert=True)
 
-@app.on_message(filters.command("start"))
+@app.on_message(filters.command("همسه"))
 async def start(client, message: Message):
     await message.reply(
-        "مرحبا\n"
-        "🌐 انا بوت اهمسلي.\n\n"
-        "💬 يمكنك استخدامي لارسال رسائل سرية (همسات) في اي مجموعة تشاء.\n"
-        "🔮 انا اعمل عن بعد, هذا يعني انك تستيط استخدامي دون الحاجة لوجودي في المجموعة.\n"
-        "💌 طريقة استخدامي سهلة جداً, قم بتحويل اي رسالة من الشخص الذي تريد ان تهمس له هنا\n\n"
-        "هناك طرق اخرى للاستخدام يمكنك رؤيتها عبر الضغط على طرق اخرى لاستخدام البوت",
+        "مرحبًا\n"
+        "🌐 أنا بوت أهمسلي.\n\n"
+        "💬 يمكنك استخدامي لإرسال رسائل سرية (همسات) في أي مجموعة تشاء.\n"
+        "🔮 أعمل عن بُعد، هذا يعني أنك تستطيع استخدامي دون الحاجة لوجودي في المجموعة.\n"
+        "💌 طريقة استخدامي سهلة جدًا، قم بتحويل أي رسالة من الشخص الذي تريد أن تهمس له هنا\n\n"
+        "هناك طرق أخرى للاستخدام يمكنك رؤيتها عبر الضغط على 'طرق أخرى لاستخدام البوت'",
         reply_markup=InlineKeyboardMarkup([[
             InlineKeyboardButton("المطور", url="t.me/F_o_x_5")
         ]])

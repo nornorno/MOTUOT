f17b93a5ed52cdd684ce28d25a174c0061952d74
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-15 00:12:59 +0300

    Update همسه.py

--- AarohiX/plugins/Management/همسه.py
+++ AarohiX/plugins/Management/همسه.py
@@ -1,11 +1,12 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-
-app = Client("my_account")
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+from MatrixMusic import app
 
 hmses = {}
-@app.on_message(filters.regex(r"^همسه$"))
-async def maker(client: Client, message: Message):
+
+@app.on_message(filters.reply & filters.regex("همسه") & filters.group)
+async def reply_with_link(client, message):
     user_id = message.reply_to_message.from_user.id
     my_id = message.from_user.id
     bar_id = message.chat.id
@@ -18,70 +19,61 @@ async def maker(client: Client, message: Message):
     await message.reply_text("\n╢ إضغط لإرسال همسه!\n", reply_markup=reply_markup, reply_to_message_id=message.message_id)
 
 waiting_for_hms = False
-
 @app.on_message(filters.command("start"), group=473)
 async def hms_start(client, message):
-    if message.text.split(" ", 1)[-1].startswith("hms"):
-        global waiting_for_hms, hms_ids
-        hms_ids = message.text
-        waiting_for_hms = True
-        await message.reply_text(
-            "-> أرسل الهمسه الآن.\n√",
-            reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("إلغاء ❌️", callback_data="hms_cancel")
-            ]])
-        )
+  if message.text.split(" ", 1)[-1].startswith("hms"):
+    global waiting_for_hms, hms_ids
+    hms_ids = message.text
+    waiting_for_hms = True
+    await message.reply_text(
+      "-> أرسل الهمسه الآن.\n√",
+      reply_markup = InlineKeyboardMarkup ([[
+        InlineKeyboardButton ("إلغاء ❌️", callback_data="hms_cancel")
+      ]])
+    )
+    return
 
 @app.on_message(filters.private & filters.text & ~filters.command("start"), group=921)
 async def send_hms(client, message):
-    global waiting_for_hms
-    if waiting_for_hms:
-        to_id = int(hms_ids.split("to")[-1].split("in")[0])
-        from_id = int(hms_ids.split("hms")[-1].split("to")[0])
-        in_id = int(hms_ids.split("in")[-1])
-        
-        hmses[str(to_id)] = {"hms": message.text, "bar": in_id}
-        
-        await message.reply_text("-> تم ارسال الهمسه.\n√")
-        
-        await app.send_message(
-            chat_id=in_id,
-            text=f"╖ لديك همسه جديدة!\n╜انت فقط من يستطيع رؤيتها 🔐",
-            reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data="hms_answer")
-            ]])
-        )
-        
-        waiting_for_hms = False
-
-@app.on_callback_query(filters.regex("hms_answer"))
-async def display_hms(client, callback: CallbackQuery):
-    in_id = callback.message.chat.id
-    who_id = callback.from_user.id
+  global waiting_for_hms
+  if waiting_for_hms:    
+    to_id = int(hms_ids.split("to")[-1].split("in")[0])
+    from_id = int(hms_ids.split("hms")[-1].split("to")[0])
+    in_id = int(hms_ids.split("in")[-1])
+    to_url = f"tg://openmessage?user_id={to_id}"
+    from_url = f"tg://openmessage?user_id={from_id}"
     
-    if hmses.get(str(who_id)) and hmses[str(who_id)]["bar"] == in_id:
-        await callback.answer(hmses[str(who_id)]["hms"], show_alert=True)
-    else:
-        await callback.answer("الأمر ليس لك", show_alert=True)
-
-@app.on_callback_query(filters.regex("hms_cancel"))
-async def cancel_hms(client, callback: CallbackQuery):
-    global waiting_for_hms
+    hmses[str(to_id)] = { "hms" : message.text, "bar" : in_id }
+    
+    await message.reply_text("-> تم ارسال الهمسه.\n√")
+    
+    await app.send_message(
+      chat_id = in_id,
+      text = f"╖ المستخدم [{(await app.get_chat(to_id)).first_name}]({to_url})\n╢ لديك همسه من البني آدم دا [{(await app.get_chat(from_id)).first_name}]({from_url})\n╜انت فقط من يستطيع رؤيتها 🔐",
+      reply_markup = InlineKeyboardMarkup ([[InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data = "hms_answer")]])
+    )
+    
     waiting_for_hms = False
+  
+@app.on_callback_query(filters.regex("hms_answer"))
+async def display_hms(client, callback):
+  in_id = callback.message.chat.id
+  who_id = callback.from_user.id
+  
+  if hmses.get(str(who_id)) is not None:
+    if hmses.get(str(who_id))["bar"] == in_id:
+      await callback.answer( hmses.get(str(who_id))["hms"], show_alert = True )
+  else:
+    await callback.answer( "الامر ليس لك ", show_alert = True )
     
-    await callback.message.edit_text("-> تم إلغاء الهمسه!\n√")
-
-@app.on_message(filters.regex(r"^همسه$"))
-async def maker(client: Client, message: Message):
-    if message.reply_to_message and message.reply_to_message.from_user.is_bot:
-        await message.reply("أنت تحاول أن تهمس للبوت؟ يا عبيط 😄")
-    elif message.from_user.id == message.chat.id:
-        await message.reply("أنت تحاول أن تهمس لنفسك؟ سلامة عقلك يا حب 😅")
-    else:
-        # متغير لتخزين الصورة المتحركة
-        animated_image = 'https://te.legra.ph/file/d0bedd7b2d959f44ae9ab.mp4'
-        # إرسال الصورة المتحركة
-        await message.reply_animation(animation=animated_image)
-
-# يجب تشغيل البوت في النهاية
-app.run()
+@app.on_callback_query(filters.regex("hms_cancel"))
+async def cancel_hms(client, callback):
+  
+  global waiting_for_hms
+  waiting_for_hms = False
+  
+  await client.edit_message_text(
+      chat_id = callback.message.chat.id,
+      message_id = callback.message.id,
+      text = "-> تم إلغاء الهمسه!\n√")
+  

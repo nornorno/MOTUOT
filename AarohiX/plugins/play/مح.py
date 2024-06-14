f6d7a7a6d99f18f9073a858cffb499ad8c8db8dd
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-14 19:16:07 +0300

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -299,7 +299,7 @@ async def maker(client: Client, message: Message):
         await client.send_message(current_spouse_id, f"@{message.from_user.username} بيحاول يتزوج عليك/عليكِ!")
     else:
         @app.on_message(filters.regex(r"^زواج$"))
-async def maker(client: Client, message: Message):
+    async def maker(client: Client, message: Message):
     try:
         if message.reply_to_message:
             reply_name = message.reply_to_message.from_user.first_name
